87b1f107fe4de1450a013144177a1033aaa94935
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 11:27:55 +0100

    Add test for Min_balance guard on touch_in

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,30 +2,30 @@ require 'oystercard'
 
 describe Oystercard do
     before :each do
-        @card = Oystercard.new(5)
+        @card = Oystercard.new(0)
     end
     describe '#check_balance' do
         it 'returns the current balance' do
-            expect(@card.balance).to eq 5
+            expect(@card.balance).to eq 0
         end
     end
     describe '#top_up' do
         context 'when the amount will not push the balance above the card limit' do
             it 'adds money to balance' do
                 @card.top_up(3)
-                expect(@card.balance).to eq 8
+                expect(@card.balance).to eq 3
             end
         end
         context 'when the amount will not push the balance above the card limit' do
             it 'adds money to balance' do
-                expect{@card.top_up(90)}.to raise_error(/Maximum balance exceeded!/)
+                expect{@card.top_up(95)}.to raise_error(/Maximum balance exceeded!/)
             end
         end
     end
     describe '#deduct' do
         it 'deducts money from the balance' do
             @card.deduct(2)
-            expect(@card.balance).to eq 3
+            expect(@card.balance).to eq -2
         end
     end
     describe '#in_journey?' do
@@ -36,13 +36,22 @@ describe Oystercard do
         end
     end
     describe '#touch_in' do
-        it 'changes the in_journey status to true' do
-            @card.touch_in
-            expect(@card.in_journey?).to eq true
+        context 'when the balance on the card is above the minimum fare' do
+            it 'changes the in_journey status to true' do
+                @card.top_up(3)
+                @card.touch_in
+                expect(@card.in_journey?).to eq true
+            end
+        end
+        context 'when there is insufficient balance on the card' do
+            it 'raises an error' do
+                expect{@card.touch_in}.to raise_error(/Insufficient balance on card!/)
+            end
         end
     end
     describe '#touch_out' do
         it 'changes the in_journey status to false' do
+            @card.top_up(5)
             @card.touch_in
             expect(@card.in_journey?).to eq true
             @card.touch_out
