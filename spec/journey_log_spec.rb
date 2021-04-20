e0b17ba11e868d55866d984a470370dbd23620d4
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 15:50:11 +0100

    Add extra assertion to check current_journey moves to journeys array

--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -4,7 +4,7 @@ describe JourneyLog do
 
     let(:barnet_station) {double('fake entry station', :name => 'High Barnet', :zone => 5)}
     let(:brixton_station) {double('fake exit station', :name => 'Brixton', :zone => 2)}
-    let(:journey) {double('fake journey object', :log_entry => nil, :log_exit => nil)}
+    let(:journey) {double('fake journey object', :log_entry => nil, :log_exit => nil, :fare => 0)}
     let(:mock_journey_class) {double('fake journey class', :new => journey)}
 
     before :each do
@@ -25,9 +25,11 @@ describe JourneyLog do
         end
     end
     describe '#finish' do
-        it 'adds an exit_station to the current journey' do
+        it 'adds an exit_station to the current journey and moves it to the journeys array' do
+            @log.start(barnet_station)
             @log.finish(brixton_station)
             expect(journey).to have_received(:log_exit).with(brixton_station)
+            expect(@log.journeys.length).to eq 1
         end
     end
 
