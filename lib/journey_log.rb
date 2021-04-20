bf8bfc0882bec657dfe44bea949e42cfea6f5b86
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 14:50:40 +0100

    Add start and finish methods

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -2,8 +2,24 @@ class JourneyLog
 
     attr_reader :journeys
 
-    def initialize
+    def initialize(journey_class:)
+        @journey_class = journey_class
         @journeys = []
+    end
+
+    def start(entry_station)
+        current_journey.log_entry(entry_station)
+    end
+
+    def finish(exit_station)
+        current_journey.log_exit(exit_station)
+    end
+
+    private 
+
+    def current_journey
+        journey = @journey_class.new
+        journey
     end
 
 end
