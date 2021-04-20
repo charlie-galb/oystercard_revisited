e14eb1256faecc4468b2ac911531c8f7f67d40c7
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 14:50:28 +0100

    Add tests for start and finish

--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -4,16 +4,30 @@ describe JourneyLog do
 
     let(:barnet_station) {double('fake entry station', :name => 'High Barnet', :zone => 5)}
     let(:brixton_station) {double('fake exit station', :name => 'Brixton', :zone => 2)}
-    let(:journey) {double('fake journey object')}
-    let(:journey_class) {double('fake journey class', :new => journey)}
+    let(:journey) {double('fake journey object', :log_entry => nil, :log_exit => nil)}
+    let(:mock_journey_class) {double('fake journey class', :new => journey)}
 
-    log = JourneyLog.new
+    before :each do
+        @log = JourneyLog.new(journey_class: mock_journey_class)
+    end
 
     describe '#journeys' do
         context 'when first initialized' do
             it 'returns an empty array' do
-                expect(log.journeys).to eq []
+                expect(@log.journeys).to eq []
             end
+        end
+    end
+    describe '#start' do
+        it 'adds an entry_station to the current journey' do
+            @log.start(barnet_station)
+            expect(journey).to have_received(:log_entry).with(barnet_station)
+        end
+    end
+    describe '#finish' do
+        it 'adds an exit_station to the current journey' do
+            @log.finish(brixton_station)
+            expect(journey).to have_received(:log_exit).with(brixton_station)
         end
     end
 
