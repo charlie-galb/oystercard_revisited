b66f508124350806541aebaaf20516c8018e5c32
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 13:48:07 +0100

    Add #fare, which returns the correct fare

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,6 +1,9 @@
 class Journey
 
     attr_reader :entry_station, :exit_station
+
+    PENALTY_FARE = 6
+    MINIMUM_FARE = 1
     
     def initialize(entry_station)
         @entry_station = entry_station
@@ -9,6 +12,10 @@ class Journey
 
     def log_exit(exit_station)
         @exit_station = exit_station
+    end
+
+    def fare
+        @exit_station ? MINIMUM_FARE : PENALTY_FARE
     end
 
 end
