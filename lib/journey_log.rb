f20cc52968cf1b914fc817d81de093e147c94768
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 15:50:45 +0100

    Add in_transit and calculate_fare functions

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,25 +1,37 @@
 class JourneyLog
 
-    attr_reader :journeys
+    attr_reader :journeys, :current_fare
 
     def initialize(journey_class:)
+        @current_fare = 0
         @journey_class = journey_class
         @journeys = []
     end
 
     def start(entry_station)
         current_journey.log_entry(entry_station)
+        calculate_fare
     end
 
     def finish(exit_station)
         current_journey.log_exit(exit_station)
+        calculate_fare
+        journeys << current_journey
+        current_journey = nil
+    end
+
+    def in_transit?
+        current_journey.in_progress?
     end
 
     private 
 
     def current_journey
-        journey = @journey_class.new
-        journey
+        @current_journey ||= @journey_class.new
+    end
+
+    def calculate_fare
+        @current_fare = current_journey.fare
     end
 
 end
