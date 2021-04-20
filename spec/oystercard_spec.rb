49eb39520646906594fa7139f4a92833431a1c1c
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 10:52:06 +0100

    HAAdd ability to hold balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
-
+    describe '#check_balance' do
+        it 'returns the current balance' do
+            card = Oystercard.new(5)
+            expect(card.balance).to eq 5
+        end
+    end
 end
