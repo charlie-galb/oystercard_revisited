ff789353ee39528aab3cae5a60a5ed7b8a558fc8
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 11:59:39 +0100

    Add test to check current entry station

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do
+
+    let(:entry_station) {double('fake entry station', :name => 'High Barnet')}
+
     before :each do
         @card = Oystercard.new(0)
     end
+
     describe '#check_balance' do
         it 'returns the current balance' do
             expect(@card.balance).to eq 0
@@ -31,29 +35,34 @@ describe Oystercard do
     end
     describe '#touch_in' do
         context 'when the balance on the card is above the minimum fare' do
-            it 'changes the in_journey status to true' do
+            it 'changes the in_journey status to true ' do
                 @card.top_up(3)
-                @card.touch_in
+                @card.touch_in(entry_station)
                 expect(@card.in_journey?).to eq true
+            end
+            it 'stores the entry station' do
+                @card.top_up(3)
+                @card.touch_in(entry_station)
+                expect(@card.current_entry_station).to eq 'High Barnet'
             end
         end
         context 'when there is insufficient balance on the card' do
             it 'raises an error' do
-                expect{@card.touch_in}.to raise_error(/Insufficient balance on card!/)
+                expect{@card.touch_in(entry_station)}.to raise_error(/Insufficient balance on card!/)
             end
         end
     end
     describe '#touch_out' do
         it 'changes the in_journey status to false' do
             @card.top_up(5)
-            @card.touch_in
+            @card.touch_in(entry_station)
             expect(@card.in_journey?).to eq true
             @card.touch_out
             expect(@card.in_journey?).to eq false
         end
         it 'deducts the correct fare from the balance' do
             @card.top_up(5)
-            @card.touch_in
+            @card.touch_in(entry_station)
             expect {@card.touch_out}.to change{@card.balance}.by(-1)
         end
     end
