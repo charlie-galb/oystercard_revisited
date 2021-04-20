3d2d07590a662a79190ef8722e0ddac11c9b4387
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 10:56:38 +0100

    Add test for top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,19 @@
 require 'oystercard'
 
 describe Oystercard do
+
+    before :each do
+        @card = Oystercard.new(5)
+    end
     describe '#check_balance' do
         it 'returns the current balance' do
-            card = Oystercard.new(5)
-            expect(card.balance).to eq 5
+            expect(@card.balance).to eq 5
+        end
+    end
+    describe '#top_up' do
+        it 'adds money to balance' do
+            @card.top_up(3)
+            expect(@card.balance).to eq 8
         end
     end
 end
