52c1405262dce2be798c0fdf37cf6948533c9051
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 11:02:44 +0100

    Add max limit for balance and guard condition

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
-
     attr_reader :balance
+
+    MAX_LIMIT = 90
 
     def initialize(balance)
         @balance = balance
     end
 
     def top_up(amount)
+        raise "Maximum balance exceeded!" if amount + balance > MAX_LIMIT
         @balance += amount
     end
     
