9bd0c6f20576f82639d0372b0882980b3370c302
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 12:23:43 +0100

    Refactoring: store entry and exit stations as hash in travel_history array, replacing entry_station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-    attr_reader :balance, :entry_station
+    attr_reader :balance, :travel_history
 
     MAX_BALANCE = 90
     MIN_BALANCE = 1
@@ -10,6 +10,7 @@ class Oystercard
         @balance = balance
         @in_journey = false
         @entry_station = nil
+        @travel_history = []
     end
 
     def top_up(amount)
@@ -18,19 +19,23 @@ class Oystercard
     end
 
     def in_journey?
-        @entry_station ? true : false
+        if @travel_history[0] && @travel_history[0].length == 1
+            true
+        else
+            false
+        end
     end
 
     def touch_in(entry_station)
         fail_if_below_min
         @in_journey = true
-        @entry_station = entry_station.name
+        @travel_history << {entry: entry_station}
     end
 
-    def touch_out
+    def touch_out(exit_station)
         @in_journey = false
         deduct
-        @entry_station = nil
+        @travel_history[0][:exit] = exit_station
     end
 
     private 
