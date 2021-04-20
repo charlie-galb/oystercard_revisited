09ff9e0ee4d929eb483172eebc3ada33062968c1
charlie-galb <galbraith.charlie@gmail.com>
2021-04-20 13:47:50 +0100

    Add test for #fare

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,13 +5,26 @@ describe Journey do
     let(:barnet_station) {double('fake entry station', :name => 'High Barnet', :zone => 5)}
     let(:brixton_station) {double('fake exit station', :name => 'Brixton', :zone => 2)}
 
-    it 'knows its entry station' do
-        journey = Journey.new(barnet_station)
-        expect(journey.entry_station).to eq barnet_station
+    before :each do
+        @journey = Journey.new(barnet_station)
     end
-    it 'knows its exit_station' do
-        journey = Journey.new(barnet_station)
-        journey.log_exit(brixton_station)
-        expect(journey.exit_station).to eq brixton_station
+
+    describe '#entry_station' do
+        it 'knows its entry station' do
+            expect(@journey.entry_station).to eq barnet_station
+        end
+    end
+    describe '#exit_station' do
+        it 'knows its exit_station' do
+            @journey.log_exit(brixton_station)
+            expect(@journey.exit_station).to eq brixton_station
+        end
+    end
+    describe '#fare' do
+        context 'when there is no exit station' do
+            it 'charges the penalty fare' do
+                expect(@journey.fare).to eq 6
+            end
+        end
     end
 end
